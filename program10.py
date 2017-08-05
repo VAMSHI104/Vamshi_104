504d8b9978b873760fe0a4a411f94b13d752b568
VAMSHI104 <vamshikrishna30dec@gmail.com>
2017-08-04 23:37:29 -0700

    my programs

---
+++ program10.py
@@ -0,0 +1,8 @@
+
+n=input( "Please Enter any Number: ")
+Count = 0
+while(Number > 0):
+    Number = Number / 10
+    Count = Count + 1
+ 
+print Count
---
+++ program9.py
@@ -0,0 +1,6 @@
+n= input('enter numb')
+sum = 0
+for x in range(0,n+1):
+      sum = sum + x
+
+print sum
